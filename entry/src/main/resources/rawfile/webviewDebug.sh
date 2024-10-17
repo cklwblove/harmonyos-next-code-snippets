29535d483f19f1864d28c627068256b68935b200
liwb <lwbhtml@163.com>
2024-10-17 11:37:45 +0800

    refactor: 💡 新增一些工具函数和调试webview的shell脚本

---
+++ entry/src/main/resources/rawfile/webviewDebug.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# 使用方法，文档参考：https://developer.huawei.com/consumer/cn/doc/harmonyos-guides-V5/web-debugging-with-devtools-V5
+# 1. 先给执行权限：chmod +x debug.sh
+# 2. 运行直接debug.sh
+
+# Initial port number
+INITIAL_PORT=9222
+
+# Get the current port number, use initial port number if not set previously
+CURRENT_PORT=${PORT:-$INITIAL_PORT}
+
+# Get the list of all PIDs that match the condition
+PID_LIST=$(hdc shell cat /proc/net/unix | grep webview_devtools_remote_ | awk -F '_' '{print $NF}')
+
+if [ -z "$PID_LIST" ]; then
+    echo "Failed to retrieve PID from the device"
+    exit 1
+fi
+
+# Increment the port number
+PORT=$CURRENT_PORT
+
+# Forward ports for each application one by one
+for PID in $PID_LIST; do
+    # Increment the port number
+    PORT=$((PORT + 1))
+
+    # Execute the hdc fport command
+    hdc fport tcp:$PORT localabstract:webview_devtools_remote_$PID
+
+    # Check if the command executed successfully
+    if [ $? -ne 0 ]; then
+        echo "Failed to execute hdc fport command"
+        exit 1
+    fi
+done
+
+# List all forwarded ports
+hdc fport ls
